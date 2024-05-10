36df91fa412517df642fcfe769a8dbcfc1357aff
hansolkim9 <masicer2@gmail.com>
2024-05-10 15:31:45 +0900

    mvc architecture part3

--- src/main/java/com/jsp/chap04/AddNewDancerServlet.java
+++ src/main/java/com/jsp/chap04/AddNewDancerServlet.java
@@ -52,8 +52,11 @@ public class AddNewDancerServlet extends HttpServlet {
         // 수송객 (page, request, session, application)
         // request: 한번의 요청과 응답이 끝날동안만 보관
         // session : 브라우저가 꺼질때까지 or 세션시간이 만료될때까지 보관
-        req.setAttribute("name", name);
-        req.setAttribute("crew", crewName);
+//        req.setAttribute("name", name);
+//        req.setAttribute("crew", crewName);
+//        req.setAttribute("level", danceLevel);
+
+        req.setAttribute("d", dancer);
 
 
         // 적당한 HTML(JSP) 응답
